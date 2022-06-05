aaaa5208903bbc7c142a17342402ea3b9772e3e3
José Nadin <93929513+josenadin01@users.noreply.github.com>
2022-06-05 11:22:06 -0300

    styling homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,38 @@
 /* eslint-disable jsx-a11y/alt-text */
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+import "../styles/auth.scss";
+    
+import { Button } from "../components/Button";
 
 export function Home() {
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div>
-                    <img src={logoImg} alt="Letmeask"/>
-                </div>
-                <button>
-                    <img src={googleIconImg} alt="Logo do Google" />
-                    Crie sua sala com o Google
-                </button>
-                <div>ou entre em uma sala</div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Digite o código da sala"
-                    />
-                    <button type="submit">
-                        Entrar na sala
-                    </button>
-                </form>
-            </main>
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
