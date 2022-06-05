cf9bc799dcc55cabef7a7065df4c8fed113441d9
José Nadin <93929513+josenadin01@users.noreply.github.com>
2022-06-05 20:15:41 -0300

    Feat: Authentication with google and its context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import {useNavigate} from 'react-router-dom'
+
 /* eslint-disable jsx-a11y/alt-text */
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -6,8 +8,21 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
     
 import { Button } from "../components/Button";
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
+  const navigate = useNavigate();
+  const { user, signInWithGoogle} = useAuth();
+
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    navigate('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -21,11 +36,10 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-
           <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
